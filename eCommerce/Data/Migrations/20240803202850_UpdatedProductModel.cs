0969f6144b9dfb9ff2d88c9d1b3439705e3d557a
Warsha Jhankri <warsajmagar@gmail.com>
2024-08-03 16:54:30 -0400

    Updated Product and Category View

---
+++ eCommerce/Data/Migrations/20240803202850_UpdatedProductModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCommerce.Data.Migrations
+{
+    public partial class UpdatedPrdocuctModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
