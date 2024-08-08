79ee1a24d7532cf309abf7478ad8084e7e666b36
Warsha Jhankri <warsajmagar@gmail.com>
2024-08-08 00:45:18 -0400

    Added Home Page View

--- eCommerce/Controllers/HomeController.cs
+++ eCommerce/Controllers/HomeController.cs
@@ -1,27 +1,45 @@
-﻿using eCommerce.Models;
+﻿using eCommerce.Data;
+using eCommerce.Models;
 using eCommerce.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace eCommerce.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
+            _context = context;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
+            // fetch a random product
+            var featuredProducts = await _context.Products
+                .Include(p => p.Category) // eagerly load the Category
+                .OrderBy(p => Guid.NewGuid()) // shuffle the products
+                .Take(3) // select 3 products
+                .ToListAsync();
 
-        public IActionResult Privacy()
-        {
-            return View();
+            var viewModels = featuredProducts.Select(p => new ProductViewModel
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Price = p.Price,
+                Description = p.Description,
+                Image = p.Image,
+                Stock = p.Stock,
+                CategoryId = p.CategoryId,
+                CategoryName = p.Category.Name
+            }).ToList();
+
+            return View(viewModels);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
